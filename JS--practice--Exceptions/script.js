f1abfbdc7b09fa085249f0d71d6187a0ecb0811b
aleksandra05sh <aleksandra05sh@gmal.com>
2021-10-03 20:06:00 +0300

    1st part for check

---
+++ JS--practice--Exceptions/script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+     
+/* Искоючения.Задание №1. Напишите функцию sumSliceArray(arr, first, second), которая принимает массив arr и два числа (first и second) – порядковые номера элементов массива, которые необходимо суммировать. Например, если ввели 3 и 5 – суммируются 3-й и 5-й элементы. Функция должна генерировать исключения если были введены не числа, и когда одно из чисел, или оба больше размера массива.
+Напишите код, который использует данную функцию, предусмотрите обработку возможных исключений.*/
+
+function sumSliceArray(arr, first, second) {
+  if (typeof first !== 'number' || typeof second !== 'number') {
+    throw new Error('Введено не число');
+  } else if (first > arr.length || second > arr.length) {
+    throw new TypeError ('Порядковый номер больше масива');
+  } else {
+    return arr[first - 1] + arr[second - 1];
+  }
+};
+
+var array = [1, 2, 3, 4, 5, 6]
+
+var res = sumSliceArray(array, 7, 3);
+console.log(res);
